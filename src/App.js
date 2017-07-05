0f8a7b7386ddad03345c117066c61cb6bda33c89
daikikuchi <kikuchi.dai@gmail.com>
2017-07-05 16:21:09 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import SearchBook from './components/SearchBook'
 import CurrentlyReading from './components/CurrentlyReading'
 import WantToRead from './components/WantToRead'
@@ -8,48 +8,30 @@ import Read from './components/Read'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-
 class BooksApp extends React.Component {
   constructor(props) {
     super(props)
     this.updateBook = this.updateBook.bind(this);
   }
 
-state = {
-      books : [],
-
-     }
-
-
-      //  updateBook(book,shelf) {
-      //   BooksAPI.update(book,shelf);
-      //   BooksAPI.getAll().then((books) => {
-      //   this.setState({ books: books })
-      // })
-      // }
-
+  state = {
+    books: []
+  }
 
   updateBook(book, shelf) {
     let self = this
     BooksAPI.update(book, shelf).then(function() {
-      BooksAPI.getAll().then(books => self.setState({
-        books: books
-      }))
-
+      BooksAPI.getAll().then(books => self.setState({books: books}))
     })
-
   }
 
-    componentDidMount() {
-      BooksAPI.getAll().then((books) => {
-        this.setState({ books }) // sames as ({books: books })  ES6
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books}) // sames as ({books: books })  ES6
+      console.log(this.state.books)
 
     })
-
-   }
-
-
-
+  }
 
   render() {
 
@@ -57,59 +39,22 @@ state = {
     let wantToRead
     let read
 
-    if(this.state.books !== null) {
-        currentlyReading = this.state.books.filter((book) => book.shelf === 'currentlyReading')
-        wantToRead = this.state.books.filter((book) => book.shelf === 'wantToRead')
-        read = this.state.books.filter((book) => book.shelf === 'read')
-
-
-      }
-
+    if (this.state.books !== null) {
+      currentlyReading = this.state.books.filter((book) => book.shelf === 'currentlyReading')
+      wantToRead = this.state.books.filter((book) => book.shelf === 'wantToRead')
+      read = this.state.books.filter((book) => book.shelf === 'read')
+    }
 
     return (
       <div className="app">
-        <Route path="/search" render={() => (
-             <SearchBook  />)}/>
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-        <Route exact path="/" render={() => (
-
-          <CurrentlyReading
-            // onDeleteContact={this.removeContact}
-            books={currentlyReading}
-            onUpdateBook={this.updateBook}
-          />
-        )}/>
-       <Route exact path="/" render={() => (
-        <WantToRead
-
-          books={wantToRead}
-          onUpdateBook={this.updateBook}
-
-        />
-      )}/>
-      <Route exact path="/" render={() => (
-       <Read
-
-         books={read}
-        //  onUpdateBook ={this.updateBook}
-        onUpdateBook={this.updateBook}
-       />
-     )}/>
-         <Route path="/rating" render={() => (
-              <Rating
-              onUpdateBook={this.props.onUpdateBook}
-
-           />)}/>
+        <Route exact path="/" render={() => (<CurrentlyReading books={currentlyReading} onUpdateBook={this.updateBook}/>)}/>
+        <Route exact path="/" render={() => (<WantToRead books={wantToRead} onUpdateBook={this.updateBook}/>)}/>
+        <Route exact path="/" render={() => (<Read books={read} onUpdateBook={this.updateBook}/>)}/>
+        <Route path="/search" render={() => (<SearchBook onUpdateBook={this.updateBook}/>)}/>
+        <Route path="/rating" render={() => (<Rating/>)}/>
       </div>
-    </div>
-
-
-     )
-
- }
+    )
+  }
 }
 
 export default BooksApp
